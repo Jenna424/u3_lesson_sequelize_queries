309e31579a3f44fd6c33ef3f9a271356b97b3843
Jenna Leopold <jleopold424@gmail.com>
2022-04-08 14:25:50 -0400

    completed lesson

--- queries.js
+++ queries.js
@@ -5,8 +5,57 @@ const stringify = (data) => {
   console.log(JSON.stringify(data, null, 2))
 }
 
+const findAllBusiness = async () => {
+  const result = await Business.findAll()
+  stringify(result)
+}
+
+const findBusinessById = async (id) => {
+  const result = await Business.findByPk(id)
+  stringify(result)
+}
+
+const findBusinessByRating = async () => {
+  const result = await Business.findAll({
+    where: { rating: 5 }
+  })
+  stringify(result)
+}
+
+const createBusiness = async () => {
+  const result = await Business.create({
+    name: 'Rando Business 2',
+    address: '1234 Main St.',
+    rating: 5
+  })
+  stringify(result)
+}
+
+const updateBusiness = async () => {
+  const result = await Business.update(
+    {
+      name: 'BETTER BUSINESS NAME'
+    },
+    { where: { name: 'RANDOM BUSINESS' } }
+  )
+  stringify(result)
+}
+
+const deleteBusiness = async () => {
+  const result = await Business.destroy({
+    where: { name: 'BETTER BUSINESS NAME' }
+  })
+  stringify(result)
+}
+
 async function main() {
   try {
+    await findAllBusiness()
+    // await findBusinessById(4)
+    // await findBusinessByRating()
+    // await createBusiness()
+    // await updateBusiness()
+    // await deleteBusiness()
   } catch (error) {
     console.log(error)
   } finally {
